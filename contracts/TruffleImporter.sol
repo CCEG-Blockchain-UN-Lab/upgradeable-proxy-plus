3135b477f9e3a9945da1c8cee39b93d85c9e23e3
ProphetDaniel <prophetdaniel@ethereumclassic.org>
2018-08-15 14:31:47 -0300

    :art: Simplify for publishing.

--- contracts/TruffleImporter.sol
+++ contracts/TruffleImporter.sol
@@ -1,7 +1,4 @@
 pragma solidity ^0.4.0;
 
-import "upgradeable-proxy/contracts/Proxy.sol";
-import "upgradeable-proxy/contracts/safe/SafeProxy.sol";
-import "upgradeable-proxy/contracts/ownable/OwnableProxy.sol";
 import "../test/test-contracts/AddressSimple/AddressSimpleV1.sol";
 import "../test/test-contracts/AddressSimple/AddressSimpleV2.sol";
